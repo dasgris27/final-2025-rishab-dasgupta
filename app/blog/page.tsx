076ac8fcf7a3d2eefab533eda1e804ee99ffc2da
dasgris27 <dasgris27@gmail.com>
2025-06-13 08:53:27 -0700

    Add files via upload

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,74 +1,72 @@
-import {title} from "@/components/primitives";
-import {Divider} from "@heroui/divider";
-import {Chip} from "@heroui/chip";
+import { Divider } from "@heroui/divider";
+import { Chip } from "@heroui/chip";
+
+import { title } from "@/components/primitives";
 
 export default function BlogPage() {
-    return (
-        <>
-            <h1 className={title()}>Blog</h1>
-            <p>
-                Welcome to my blog. Here you can find all of the latest news
-                about the projects I have been working on, and other things that
-                are going on in my life.
-            </p>
-            <Divider className="my-8" />
-            <div className="flex gap-2 md:gap-4 md:items-center flex-col md:flex-row mb-4 md:mb-0">
-                <h2 className={title({size: "sm"})} id="ela-struggles">
-                    Struggles in Language Arts
-                </h2>
-                <div className="flex gap-2">
-                    <Chip>Educational</Chip>
-                    <Chip>Professional</Chip>
-                    <Chip>Teaser</Chip>
-                </div>
-            </div>
-            <p className="mb-2 opacity-75">June 11, 2025</p>
-            <p className="mb-2">
-                As you might already know, it is nearing the end of the school
-                year, which might sound really fun. I mean, summer is coming
-                soon, a well-deserved break is approaching, and I can kiss
-                goodbye to all that annoying homework, right? Well, it turns out
-                that this is the prime time for teachers to just squeeze in
-                huge, unbelievably large projects that make you think "Man, I
-                just want school to be over already." It is no different in my
-                English class, where we have to make a project on contemporary
-                issues, where we not only have to do literary analysis on
-                basically everything about some movie, but also make a "creative
-                piece" to "show our understanding," as if I haven't shown it
-                already with the seven pages I have written. This is actually
-                getting so ridiculous at this point.
-            </p>
-            <p>
-                Because of all this work given to me, you might not see me
-                working on any projects for the next few weeks, but I promise,
-                when I share my next project with all of you, you will be
-                fantastically surprised. I'm not going to tell you exactly what
-                I am planning, but I promise that it will be fun and
-                interactive. I'll give you a four-word hint as to what it is:{" "}
-                <span className="font-bold">build whatever you want</span>.
-            </p>
-            <Divider className="my-8" />
-            <div className="flex gap-2 md:gap-4 md:items-center flex-col md:flex-row mb-4 md:mb-0">
-                <h2 className={title({size: "sm"})} id="starting-portfolio">
-                    Starting My Portfolio
-                </h2>
-                <div className="flex gap-2">
-                    <Chip>Website</Chip>
-                    <Chip>Project</Chip>
-                    <Chip>Programming</Chip>
-                </div>
-            </div>
-            <p className="mb-2 opacity-75">June 9, 2025</p>
-            <p>
-                Hello, this is the first blog post that I am writing on my
-                portfolio, to announce that I am finally working on my portfolio
-                to showcase my work to others. I am currently having a little
-                bit of trouble thinking of what to write on my main page, but
-                otherwise it is going pretty well so far. I hope I can get to a
-                decent point by Friday. I cannot wait for all of you to see the
-                final product!
-            </p>
-            <Divider className="my-8" />
-        </>
-    );
+  return (
+    <>
+      <h1 className={title()}>Blog</h1>
+      <p>
+        Welcome to my blog. Here you can find all of the latest news about the
+        projects I have been working on, and other things that are going on in
+        my life.
+      </p>
+      <Divider className="my-8" />
+      <div className="flex gap-2 md:gap-4 md:items-center flex-col md:flex-row mb-4 md:mb-0">
+        <h2 className={title({ size: "sm" })} id="ela-struggles">
+          Struggles in Language Arts
+        </h2>
+        <div className="flex gap-2">
+          <Chip>Educational</Chip>
+          <Chip>Professional</Chip>
+          <Chip>Teaser</Chip>
+        </div>
+      </div>
+      <p className="mb-2 opacity-75">June 11, 2025</p>
+      <p className="mb-2">
+        As you might already know, it is nearing the end of the school year,
+        which might sound really fun. I mean, summer is coming soon, a
+        well-deserved break is approaching, and I can kiss goodbye to all that
+        annoying homework, right? Well, it turns out that this is the prime time
+        for teachers to just squeeze in huge, unbelievably large projects that
+        make you think &quot;Man, I just want school to be over already.&quot;
+        It is no different in my English class, where we have to make a project
+        on contemporary issues, where we not only have to do literary analysis
+        on basically everything about some movie, but also make a &quot;creative
+        piece&quot; to &quot;show our understanding,&quot; as if I haven&apos;t
+        shown it already with the seven pages I have written. This is actually
+        getting so ridiculous at this point.
+      </p>
+      <p>
+        Because of all this work given to me, you might not see me working on
+        any projects for the next few weeks, but I promise, when I share my next
+        project with all of you, you will be fantastically surprised. I&apos;m
+        not going to tell you exactly what I am planning, but I promise that it
+        will be fun and interactive. I&apos;ll give you a four-word hint as to
+        what it is: <span className="font-bold">build whatever you want</span>.
+      </p>
+      <Divider className="my-8" />
+      <div className="flex gap-2 md:gap-4 md:items-center flex-col md:flex-row mb-4 md:mb-0">
+        <h2 className={title({ size: "sm" })} id="starting-portfolio">
+          Starting My Portfolio
+        </h2>
+        <div className="flex gap-2">
+          <Chip>Website</Chip>
+          <Chip>Project</Chip>
+          <Chip>Programming</Chip>
+        </div>
+      </div>
+      <p className="mb-2 opacity-75">June 9, 2025</p>
+      <p>
+        Hello, this is the first blog post that I am writing on my portfolio, to
+        announce that I am finally working on my portfolio to showcase my work
+        to others. I am currently having a little bit of trouble thinking of
+        what to write on my main page, but otherwise it is going pretty well so
+        far. I hope I can get to a decent point by Friday. I cannot wait for all
+        of you to see the final product!
+      </p>
+      <Divider className="my-8" />
+    </>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,91 +1,90 @@
 "use client";
 
-import {title, subtitle} from "@/components/primitives";
-import {ChevronDown, SquareArrowOutUpRight} from "lucide-react";
-import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card";
-import {Button} from "@heroui/button";
-import {Link} from "@heroui/link";
+import { ChevronDown, SquareArrowOutUpRight } from "lucide-react";
+import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 import {
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    DropdownItem,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
 } from "@heroui/dropdown";
 
+import { title, subtitle } from "@/components/primitives";
+
 export default function ProjectsPage() {
-    return (
-        <div className="relative">
-            <div className="flex flex-col lg:flex-row gap-4">
-                <Dropdown className="lg:hidden w-full">
-                    <DropdownTrigger>
-                        <Button className="lg:hidden">
-                            <span>View Project Categories</span>
-                            <ChevronDown />
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu>
-                        <DropdownItem key="games" href="#games">
-                            Games
-                        </DropdownItem>
-                        {/* <DropdownItem key="tools" href="#tools">
+  return (
+    <div className="relative">
+      <div className="flex flex-col lg:flex-row gap-4">
+        <Dropdown className="lg:hidden w-full">
+          <DropdownTrigger>
+            <Button className="lg:hidden">
+              <span>View Project Categories</span>
+              <ChevronDown />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem key="games" href="#games">
+              Games
+            </DropdownItem>
+            {/* <DropdownItem key="tools" href="#tools">
                             Tools
                         </DropdownItem> */}
-                        <DropdownItem key="design-class" href="#design-class">
-                            Website Design Class
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-                <div className="w-64 h-full flex-shrink-0 sticky top-16 self-start p-2 hidden lg:block">
-                    <h3 className={subtitle()}>Categories</h3>
-                    <p>
-                        <Link href="#games">Games</Link>
-                    </p>
-                    {/* <p>
+            <DropdownItem key="design-class" href="#design-class">
+              Website Design Class
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <div className="w-64 h-full flex-shrink-0 sticky top-16 self-start p-2 hidden lg:block">
+          <h3 className={subtitle()}>Categories</h3>
+          <p>
+            <Link href="#games">Games</Link>
+          </p>
+          {/* <p>
                         <Link href="#tools">Tools</Link>
                     </p> */}
-                    <p>
-                        <Link href="#design-class">Website Design Class</Link>
-                    </p>
-                </div>
-                <div className="md:flex-grow">
-                    <h1 className={title()}>My Projects</h1>
-                    <div className="mb-4"></div>
-                    <h2 className={title({size: "sm"})} id="games">
-                        Games
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 mb-12 gap-4">
-                        <Card id="games-minesweeper-remake">
-                            <CardHeader className="p-0">
-                                <img
-                                    className="w-full aspect-square object-cover"
-                                    src="dasgris27.github.io_minesweeper.html.png"
-                                ></img>
-                            </CardHeader>
-                            <CardBody className="pb-0">
-                                <h3 className={subtitle()}>
-                                    Minesweeper Recreation
-                                </h3>
-                                <p>
-                                    A recreation of the classic game with the
-                                    same rules: reveal all of the cells as
-                                    quickly as you can without exposing any
-                                    mines.
-                                </p>
-                            </CardBody>
-                            <CardFooter>
-                                <Button
-                                    as={Link}
-                                    className="w-full"
-                                    href="https://dasgris27.github.io/minesweeper.html"
-                                    target="_blank"
-                                >
-                                    <span>Open in new tab</span>
-                                    <SquareArrowOutUpRight />
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    </div>
-                    {/* <h2 className={title({size: "sm"})} id="tools">
+          <p>
+            <Link href="#design-class">Website Design Class</Link>
+          </p>
+        </div>
+        <div className="md:flex-grow">
+          <h1 className={title()}>My Projects</h1>
+          <div className="mb-4" />
+          <h2 className={title({ size: "sm" })} id="games">
+            Games
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 mb-12 gap-4">
+            <Card id="games-minesweeper-remake">
+              <CardHeader className="p-0">
+                <img
+                  alt="Minesweeper Remake"
+                  className="w-full aspect-square object-cover"
+                  src="dasgris27.github.io_minesweeper.html.png"
+                />
+              </CardHeader>
+              <CardBody className="pb-0">
+                <h3 className={subtitle()}>Minesweeper Recreation</h3>
+                <p>
+                  A recreation of the classic game with the same rules: reveal
+                  all of the cells as quickly as you can without exposing any
+                  mines.
+                </p>
+              </CardBody>
+              <CardFooter>
+                <Button
+                  as={Link}
+                  className="w-full"
+                  href="https://dasgris27.github.io/minesweeper.html"
+                  target="_blank"
+                >
+                  <span>Open in new tab</span>
+                  <SquareArrowOutUpRight />
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+          {/* <h2 className={title({size: "sm"})} id="tools">
                         Tools
                     </h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 mb-12 gap-4">
@@ -134,80 +133,75 @@ export default function ProjectsPage() {
                             </CardBody>
                         </Card>
                     </div> */}
-                    <h2 className={title({size: "sm"})} id="design-class">
-                        Website Design Class
-                    </h2>
-                    <p>
-                        These are websites that I made while learning web design
-                        in Skyline High School.{" "}
-                        <Link href="https://github.com/bbl4kney">Blakney</Link>{" "}
-                        was my teacher, and he taught us how TailwindCSS works,
-                        ways to use it to style elements, and how to run a live
-                        server to ease the development process.
-                    </p>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 mb-12 gap-4">
-                        <Card id="design-class-may-2025-notebook-check">
-                            <CardHeader className="p-0">
-                                <img
-                                    className="w-full aspect-square object-cover"
-                                    src="yang-_9dAwWA4LD8-unsplash.jpg"
-                                ></img>
-                            </CardHeader>
-                            <CardBody className="pb-0">
-                                <h3 className={subtitle()}>
-                                    May 2025 Notebook Check
-                                </h3>
-                                <p>
-                                    Our class' progress in May 2025. This
-                                    includes a fake news page, to-do list,
-                                    practice with Tailwind, and a mockup for a
-                                    petting zoo website.
-                                </p>
-                            </CardBody>
-                            <CardFooter>
-                                <Button
-                                    as={Link}
-                                    className="w-full"
-                                    href="https://may-2025-rishab-dasgupta.vercel.app/"
-                                    target="_blank"
-                                >
-                                    <span>Open in new tab</span>
-                                    <SquareArrowOutUpRight />
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                        <Card id="design-class-march-2025-notebook-check">
-                            <CardHeader className="p-0">
-                                <img
-                                    className="w-full aspect-square object-cover"
-                                    src="shayan-ramesht-exSEmuA7R7k-unsplash.jpg"
-                                ></img>
-                            </CardHeader>
-                            <CardBody className="pb-0">
-                                <h3 className={subtitle()}>
-                                    March 2025 Notebook Check
-                                </h3>
-                                <p>
-                                    Our class' progress in March 2025. Includes
-                                    a basic About Me page, list of recipes, and
-                                    a fun fact generator.
-                                </p>
-                            </CardBody>
-                            <CardFooter>
-                                <Button
-                                    as={Link}
-                                    className="w-full"
-                                    href="https://march-2025-rishab-dasgupta.vercel.app/"
-                                    target="_blank"
-                                >
-                                    <span>Open in new tab</span>
-                                    <SquareArrowOutUpRight />
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    </div>
-                </div>
-            </div>
+          <h2 className={title({ size: "sm" })} id="design-class">
+            Website Design Class
+          </h2>
+          <p>
+            These are websites that I made while learning web design in Skyline
+            High School. <Link href="https://github.com/bbl4kney">Blakney</Link>{" "}
+            was my teacher, and he taught us how TailwindCSS works, ways to use
+            it to style elements, and how to run a live server to ease the
+            development process.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 mb-12 gap-4">
+            <Card id="design-class-may-2025-notebook-check">
+              <CardHeader className="p-0">
+                <img
+                  alt="May 2025 Notebook Check"
+                  className="w-full aspect-square object-cover"
+                  src="yang-_9dAwWA4LD8-unsplash.jpg"
+                />
+              </CardHeader>
+              <CardBody className="pb-0">
+                <h3 className={subtitle()}>May 2025 Notebook Check</h3>
+                <p>
+                  Our class&apos; progress in May 2025. This includes a fake
+                  news page, to-do list, practice with Tailwind, and a mockup
+                  for a petting zoo website.
+                </p>
+              </CardBody>
+              <CardFooter>
+                <Button
+                  as={Link}
+                  className="w-full"
+                  href="https://may-2025-rishab-dasgupta.vercel.app/"
+                  target="_blank"
+                >
+                  <span>Open in new tab</span>
+                  <SquareArrowOutUpRight />
+                </Button>
+              </CardFooter>
+            </Card>
+            <Card id="design-class-march-2025-notebook-check">
+              <CardHeader className="p-0">
+                <img
+                  alt="March 2025 Notebook Check"
+                  className="w-full aspect-square object-cover"
+                  src="shayan-ramesht-exSEmuA7R7k-unsplash.jpg"
+                />
+              </CardHeader>
+              <CardBody className="pb-0">
+                <h3 className={subtitle()}>March 2025 Notebook Check</h3>
+                <p>
+                  Our class&apos; progress in March 2025. Includes a basic About
+                  Me page, list of recipes, and a fun fact generator.
+                </p>
+              </CardBody>
+              <CardFooter>
+                <Button
+                  as={Link}
+                  className="w-full"
+                  href="https://march-2025-rishab-dasgupta.vercel.app/"
+                  target="_blank"
+                >
+                  <span>Open in new tab</span>
+                  <SquareArrowOutUpRight />
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
